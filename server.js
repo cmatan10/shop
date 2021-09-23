0eb7e3e82b9a3984c222882d3b08339275b233a9
cmatan10 <cmatan10@gmail.com>
2021-09-23 18:14:34 +0300

    bbbbbb

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const shortid = require("shortid");
 const app = express();
 app.use(bodyParser.json());
 
-//aaaaaaaa
+//aaaaaaaaaaaa
 mongoose.connect(
   process.env.MONGODB_URL || "mongodb://localhost/react-shopping-cart-db",
   {
