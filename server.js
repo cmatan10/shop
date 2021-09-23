e64928ecc9f862bbc38589c983523d746b26ccd5
cmatan10 <cmatan10@gmail.com>
2021-09-23 18:11:36 +0300

    blibli

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const shortid = require("shortid");
 const app = express();
 app.use(bodyParser.json());
 
-//aa
+//aaaaaaaa
 mongoose.connect(
   process.env.MONGODB_URL || "mongodb://localhost/react-shopping-cart-db",
   {
